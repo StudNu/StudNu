fe904b7af116cd8d539bb94e3f33dbd0cc5df528
Олекса Соколов <aleksport1@gmail.com>
2022-07-01 01:15:10 +0300

    SOP-67 Implemented backend of adding product to cart

--- MVC_Store/Controllers/CartController.cs
+++ MVC_Store/Controllers/CartController.cs
@@ -75,5 +75,125 @@ namespace MVC_Store.Controllers
             // Возвращаем частичное представление с моделью
             return PartialView("_CartPartial", model);
         }
+
+        public ActionResult AddToCartPartial(int id)
+        {
+            // Объявляем лист, параметризированный типом CartVM
+            List<CartVM> cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+
+            // Объявляем модель CartVM
+            CartVM model = new CartVM();
+
+            using (Db db = new Db())
+            {
+                // Получаем продукт
+                ProductDTO product = db.Products.Find(id);
+
+                // Проверяем, находится ли товар уже в корзине
+                var productInCart = cart.FirstOrDefault(x => x.ProductId == id);
+
+                // Если нет, то добавляем новый товар в корзину
+                if (productInCart == null)
+                {
+                    cart.Add(new CartVM()
+                    {
+                        ProductId = product.Id,
+                        ProductName = product.Name,
+                        Quantity = 1,
+                        Price = product.Price,
+                        Image = product.ImageName
+                    });
+                }
+                // Если да, добавляем единицу товара
+                else
+                {
+                    productInCart.Quantity++;
+                }
+            }
+            // Получаем общее количество, цену и добавляем данные в модель
+            int qty = 0;
+            decimal price = 0m;
+
+            foreach (var item in cart)
+            {
+                qty += item.Quantity;
+                price += item.Quantity * item.Price;
+            }
+
+            model.Quantity = qty;
+            model.Price = price;
+
+            // Сохраняем состояние корзины в сессию
+            Session["cart"] = cart;
+
+            // Возвращаем частичное представление с моделью
+            return PartialView("_AddToCartPartial", model);
+        }
+
+
+        // GET: /cart/IncrementProduct
+        public JsonResult IncrementProduct(int productId)
+        {
+            // Объявляем лист cart
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                // Получаем модель CartVM из листа
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                // Добавляем количество
+                model.Quantity++;
+
+                // Сохраняем необходимые данные
+                var result = new { qty = model.Quantity, price = model.Price };
+
+                // Возвращаем JSON ответ с данными
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        // GET: /cart/DecrementProduct
+        public ActionResult DecrementProduct(int productId)
+        {
+            // Объявляем лист cart
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                // Получаем модель CartVM из листа
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                // Отнимаем количество
+                if (model.Quantity > 1)
+                    model.Quantity--;
+                else
+                {
+                    model.Quantity = 0;
+                    cart.Remove(model);
+                }
+
+                // Сохраняем необходимые данные
+                var result = new { qty = model.Quantity, price = model.Price };
+
+                // Возвращаем JSON ответ с данными
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public void RemoveProduct(int productId)
+        {
+            // Объявляем лист cart
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                // Получаем модель CartVM из листа
+                CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+                cart.Remove(model);
+            }
+        }
     }
 }
