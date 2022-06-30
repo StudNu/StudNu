10b4947d41694b3e24d3455416f00ffd182d0ff6
Олекса Соколов <aleksport1@gmail.com>
2022-07-01 01:03:34 +0300

    SOP-65 Implemented backend for cart

---
+++ MVC_Store/Controllers/CartController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Cart;
+
+namespace MVC_Store.Controllers
+{
+    public class CartController : Controller
+    {
+        
+        // GET: Cart
+        public ActionResult Index()
+        {
+            // Объявляем лист типа CartVM
+            var cart = Session["cart"] as List<CartVM> ?? new List<CartVM>();
+
+            // Проверяем, пуста ли корзина
+            if (cart.Count == 0 || Session["cart"] == null)
+            {
+                ViewBag.Message = "Your cart is empty.";
+                return View();
+            }
+            // Складываем сумму и записываем во ViewBag
+            decimal total = 0m;
+
+            foreach (var item in cart)
+            {
+                total += item.Total;
+            }
+
+            ViewBag.GrandTotal = total;
+
+            // Возвращаем лист в представление
+            return View(cart);
+        }
+
+        public ActionResult CartPartial()
+        {
+            // Объявляем модель CartVM
+            CartVM model = new CartVM();
+
+            // Объявляем переменную количества
+            int qty = 0;
+
+            // Объявляем переменную цены
+            decimal price = 0m;
+
+            // Проверяем сессию корзины
+            if (Session["cart"] != null)
+            {
+                // Получаем общее количество товаров и цену
+                var list = (List<CartVM>) Session["cart"];
+
+                foreach (var item in list)
+                {
+                    qty += item.Quantity;
+                    price += item.Quantity * item.Price;
+                }
+
+                model.Quantity = qty;
+                model.Price = price;
+            }
+            else
+            {
+                // Или устанавливаем количество и цену в 0
+                model.Quantity = 0;
+                model.Price = 0m;
+            }
+
+            // Возвращаем частичное представление с моделью
+            return PartialView("_CartPartial", model);
+        }
+    }
+}
